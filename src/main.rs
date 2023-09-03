4a45f9ffabfca6b8cb2d47a0a9bd719ec881cf9b
Shane Kennedy <shane.kennedy19@gmail.com>
2023-09-03 16:39:38 +0200

    Add the rust tutorial webserver

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 use std::fs;
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
 
 use md::Renderer;
+use md::ThreadPool;
 
 fn main() {
+    bootstrap_content();
+    start_webserver();
+}
+
+fn bootstrap_content() {
     let content_dir: fs::ReadDir = fs::read_dir("content").expect("Unable to read dir 'content'");
     let mut post_titles = Vec::new();
     for item in content_dir {
@@ -15,4 +26,47 @@ fn main() {
     }
 
     Renderer::render_home(post_titles);
+}
+
+fn start_webserver() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+    println!("Shutting down.");
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+
+    stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
