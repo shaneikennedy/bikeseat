a2190811fbfdd0fe0645d7881b06d884f736f390
Shane Kennedy <shane.kennedy19@gmail.com>
2023-09-03 15:41:26 +0200

    Create dumb home page with link to content

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,25 @@ use md::Parser;
 
 fn main() {
     let content_dir: fs::ReadDir = fs::read_dir("content").expect("Unable to read dir 'content'");
+    let mut post_titles = Vec::new();
     for item in content_dir {
         let file_name = item.as_ref().unwrap().file_name().into_string().unwrap();
         println!("content/{}", file_name);
-        if file_name.split(".").last().expect("last") == "md" {
+        if file_name.split(".").last().unwrap() == "md" {
             render_post(&file_name);
+            post_titles.push(file_name);
         }
     }
+
+    let mut html_str = String::new();
+    for title in post_titles {
+        html_str += &format!("<li><a href='{}.html'>{}</a></li>\n", title, title);
+    }
+    let template =
+        fs::read_to_string("templates/index.template.html").expect("could not find template file");
+    let content = template.as_str().replace("{{ html_template }}", &html_str);
+    let out_file = "out/index.html";
+    fs::write(out_file, content).expect("unable to write to index.html");
 }
 
 fn render_post(file_name: &String) {
