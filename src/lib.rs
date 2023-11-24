978090d6eec233930390a513f786c079a8d4645a
Shane Kennedy <shane.kennedy19@gmail.com>
2023-11-24 16:24:18 +0100

    Use a real web framework

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,5 @@
 use regex::Regex;
 use std::fs;
-use std::{
-    sync::{mpsc, Arc, Mutex},
-    thread,
-};
 
 const H1: &str = "# ";
 const H2: &str = "## ";
@@ -231,93 +227,5 @@ impl Renderer {
         let out_file = &format!("out/{}.html", post.split(".").nth(0).unwrap());
         fs::write(out_file, content).expect("unable to write to index.html");
         println!("Successfully generated {}", out_file.clone());
-    }
-}
-
-pub struct ThreadPool {
-    workers: Vec<Worker>,
-    sender: Option<mpsc::Sender<Job>>,
-}
-
-type Job = Box<dyn FnOnce() + Send + 'static>;
-
-impl ThreadPool {
-    /// Create a new ThreadPool.
-    ///
-    /// The size is the number of threads in the pool.
-    ///
-    /// # Panics
-    ///
-    /// The `new` function will panic if the size is zero.
-    pub fn new(size: usize) -> ThreadPool {
-        assert!(size > 0);
-
-        let (sender, receiver) = mpsc::channel();
-
-        let receiver = Arc::new(Mutex::new(receiver));
-
-        let mut workers = Vec::with_capacity(size);
-
-        for id in 0..size {
-            workers.push(Worker::new(id, Arc::clone(&receiver)));
-        }
-
-        ThreadPool {
-            workers,
-            sender: Some(sender),
-        }
-    }
-
-    pub fn execute<F>(&self, f: F)
-    where
-        F: FnOnce() + Send + 'static,
-    {
-        let job = Box::new(f);
-
-        self.sender.as_ref().unwrap().send(job).unwrap();
-    }
-}
-
-impl Drop for ThreadPool {
-    fn drop(&mut self) {
-        drop(self.sender.take());
-
-        for worker in &mut self.workers {
-            println!("Shutting down worker {}", worker.id);
-
-            if let Some(thread) = worker.thread.take() {
-                thread.join().unwrap();
-            }
-        }
-    }
-}
-
-struct Worker {
-    id: usize,
-    thread: Option<thread::JoinHandle<()>>,
-}
-
-impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(move || loop {
-            let message = receiver.lock().unwrap().recv();
-
-            match message {
-                Ok(job) => {
-                    println!("Worker {id} got a job; executing.");
-
-                    job();
-                }
-                Err(_) => {
-                    println!("Worker {id} disconnected; shutting down.");
-                    break;
-                }
-            }
-        });
-
-        Worker {
-            id,
-            thread: Some(thread),
-        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,40 @@
-use std::fs;
-use std::io::prelude::*;
-use std::net::TcpListener;
-use std::net::TcpStream;
-use std::str::from_utf8;
-
+use actix_files;
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use md::Renderer;
-use md::ThreadPool;
-use regex::Regex;
+use std::fs;
 
-fn main() {
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     bootstrap_content();
-    start_webserver();
+    HttpServer::new(|| {
+        App::new()
+            .service(home)
+            .service(blog_post)
+            .service(actix_files::Files::new("static", "./static").show_files_listing())
+    })
+    .bind(("127.0.0.1", 7878))?
+    .run()
+    .await
+}
+
+#[get("/{post_slug}")]
+async fn blog_post(post_slug: web::Path<String>) -> impl Responder {
+    let contents = match fs::read_to_string(format!("out/{}.html", post_slug)) {
+        Ok(file) => file,
+        Err(_) => fs::read_to_string("static/404.html").unwrap(),
+    };
+
+    HttpResponse::Ok().body(contents)
+}
+
+#[get("/")]
+async fn home() -> impl Responder {
+    let contents = match fs::read_to_string("out/index.html") {
+        Ok(file) => file,
+        Err(_) => fs::read_to_string("static/404.html").unwrap(),
+    };
+
+    HttpResponse::Ok().body(contents)
 }
 
 fn bootstrap_content() {
@@ -24,52 +48,5 @@ fn bootstrap_content() {
             post_titles.push(file_name);
         }
     }
-
     Renderer::render_home(post_titles);
-}
-
-fn start_webserver() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(4);
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        pool.execute(|| {
-            handle_connection(stream);
-        });
-    }
-    println!("Shutting down.");
-}
-
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
-
-    let home = b"GET / HTTP/1.1\r\n";
-    let re = Regex::new(r"GET /(?P<route>[a-zA-Z0-9-]+) HTTP/1.1\r\n").unwrap();
-    let req = from_utf8(&buffer).unwrap();
-    let (status_line, filename) = if buffer.starts_with(home) {
-        ("HTTP/1.1 200 OK", "index")
-    } else if re.is_match(req) {
-        let route = re.captures(req).unwrap().name("route").unwrap().as_str();
-        ("HTTP/1.1 200 OK", route)
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "static/404.html")
-    };
-
-    let contents = match fs::read_to_string(format!("out/{}.html", filename)) {
-        Ok(file) => file,
-        Err(_) => fs::read_to_string("static/404.html").unwrap(),
-    };
-
-    let response = format!(
-        "{}\r\nContent-Length: {}\r\n\r\n{}",
-        status_line,
-        contents.len(),
-        contents
-    );
-
-    stream.write_all(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
 }
